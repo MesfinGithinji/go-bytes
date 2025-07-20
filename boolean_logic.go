242ea8b844f61fcbfe6799ef58ed161841a1d78d
MesfinGithinji <meshgithinji@gmail.com>
2025-07-21 01:46:03 +0300

    feat: Understanding Go's boolean logic and also comparison operators

---
+++ boolean_logic.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	a, b := 30, 20
+
+	fmt.Println(a == b) //prints false
+	fmt.Println(a != b) //prints true
+	fmt.Println(a < b)  //prints false
+	fmt.Println(a > b)  //prints true
+
+}
