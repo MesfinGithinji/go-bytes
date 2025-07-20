eace307d5da6930b5c7eb3fe39145a644469196a
MesfinGithinji <meshgithinji@gmail.com>
2025-07-21 01:38:35 +0300

    feat: build a simple caculator , also practice my string output building using Sprintf

---
+++ basic_calc.go
@@ -0,0 +1,50 @@
+package main
+
+/*
+ Use variables and constants to:
+
+    Take two numbers (you can hardcode them).
+
+    Perform: addition, subtraction, multiplication, and division.
+
+    Print results with formatted strings like "Sum of %d and %d is %d"
+*/
+
+import "fmt"
+
+func main() {
+	//declare my two variables
+	num1 := 100
+	num2 := 40
+
+	//addition
+	sum := num1 + num2
+	result := fmt.Sprintf("The Sum of %d and %d is = %d", num1, num2, sum)
+	fmt.Println(result)
+
+	//subtraction
+	diff := num1 - num2
+	result2 := fmt.Sprintf("The Difference of %d and %d is = %d", num1, num2, diff)
+	fmt.Println(result2)
+
+	//multiplication
+	prod := num1 * num2
+	result3 := fmt.Sprintf("The Product of %d and %d is = %d", num1, num2, prod)
+	fmt.Println(result3)
+
+	//division
+	div := float64(num1) / float64(num2)
+	result4 := fmt.Sprintf("%d divide by %d is = %.2f", num1, num2, div)
+	fmt.Println(result4)
+
+	/*
+	 This would also work
+	 num1, num2 := 100, 40
+
+	 fmt.Printf("Sum of %d and %d is %d\n", num1, num2, num1+num2)
+	 fmt.Printf("Difference of %d and %d is %d\n", num1, num2, num1-num2)
+	 fmt.Printf("Product of %d and %d is %d\n", num1, num2, num1*num2)
+	 fmt.Printf("Quotient of %d divided by %d is %d\n", num1, num2, num1/num2)
+	 could also convert to float64 for better accuracy
+	*/
+}
