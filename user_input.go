54f0e70b09277fcce6744aedef09570fe6cedc1c
MesfinGithinji <meshgithinji@gmail.com>
2025-07-29 16:12:05 +0300

    feat: Asking user for input

---
+++ user_input.go
@@ -0,0 +1,12 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	var myName string
+// 	fmt.Print("Please Enter your name: ")
+// 	fmt.Scanln(&myName)
+
+// 	fmt.Println("Your First Name is ", myName)
+// }
